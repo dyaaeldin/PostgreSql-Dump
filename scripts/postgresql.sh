d8cc983c7c149aa27c523102f8227e3c951f45c7
Dyaa Ahmed <79382618+dyaaeldin@users.noreply.github.com>
2023-03-21 07:34:05 +0200

    Update postgresql.sh

--- scripts/postgresql.sh
+++ scripts/postgresql.sh
@@ -1,40 +1,56 @@
 #!/usr/bin/env sh
 
+set -e
+
 # Define the date
 DATE=$(date +%Y%m%d-%H-%M)
-# Create backup directories if not exists
-[ -d /tmp/$POSTGRESQL_BACKUP_DIR ] || mkdir /tmp/$POSTGRESQL_BACKUP_DIR
+
+# Create backup directories if they don't exist
+mkdir -p /tmp/"$POSTGRESQL_BACKUP_DIR"
 cd /tmp/"$POSTGRESQL_BACKUP_DIR"
 
 # Performing backup
 if [ "$BACKUP_ALL" = "true" ]; then
-    PGPASSWORD="$POSTGRESQL_PASSWORD" pg_dumpall -h $POSTGRESQL_SERVICE_NAME \
-    -p $POSTGRESQL_SERVICE_PORT -U $POSTGRESQL_USERNAME \
-    > /tmp/"$POSTGRESQL_BACKUP_DIR"/postgresql-all-$DATE.sql
-    tar cvf postgresql-all-$DATE.tar postgresql-all-$DATE.sql
+  pg_dumpall \
+    --dbname="$POSTGRESQL_DB_NAME" \
+    --host="$POSTGRESQL_SERVICE_NAME" \
+    --port="$POSTGRESQL_SERVICE_PORT" \
+    --username="$POSTGRESQL_USERNAME" \
+    --no-password \
+    --file="/tmp/$POSTGRESQL_BACKUP_DIR/postgresql-all-$DATE.sql"
+
+  tar -cvf "/tmp/$POSTGRESQL_BACKUP_DIR/postgresql-all-$DATE.tar" \
+    "/tmp/$POSTGRESQL_BACKUP_DIR/postgresql-all-$DATE.sql"
 else
-    PGPASSWORD="$POSTGRESQL_PASSWORD" pg_dump -h $POSTGRESQL_SERVICE_NAME \
-    -p $POSTGRESQL_SERVICE_PORT -U $POSTGRESQL_USERNAME $POSTGRESQL_DB_NAME \
-    > /tmp/"$POSTGRESQL_BACKUP_DIR"/postgresql-$DATE.sql
-    tar cvf postgresql-$DATE.tar postgresql-$DATE.sql
+  pg_dump \
+    --dbname="$POSTGRESQL_DB_NAME" \
+    --host="$POSTGRESQL_SERVICE_NAME" \
+    --port="$POSTGRESQL_SERVICE_PORT" \
+    --username="$POSTGRESQL_USERNAME" \
+    --no-password \
+    --file="/tmp/$POSTGRESQL_BACKUP_DIR/postgresql-$DATE.sql"
+
+  tar -cvf "/tmp/$POSTGRESQL_BACKUP_DIR/postgresql-$DATE.tar" \
+    "/tmp/$POSTGRESQL_BACKUP_DIR/postgresql-$DATE.sql"
 fi
 
-# Archicing the backup directory and copying the backup to persistent volume
-
-cp postgresql-*.tar $VOLUME_DIR
+# Archiving the backup directory and copying the backup to persistent volume
+cp "/tmp/$POSTGRESQL_BACKUP_DIR/postgresql-$DATE.tar" "$VOLUME_DIR"
 
 # Send the backup to external backup server if needed 
 if [ "$REMOTE_BACKUP" = "ssh" ]; then
-    scp -o StrictHostKeyChecking=no -P $REMOTE_SERVER_PORT -i $KEY_PATH postgresql-*.tar \
-    $REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR
+  scp -o StrictHostKeyChecking=no -P "$REMOTE_SERVER_PORT" -i "$KEY_PATH" \
+    "/tmp/$POSTGRESQL_BACKUP_DIR/postgresql-$DATE.tar" \
+    "$REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR"
 elif [ "$REMOTE_BACKUP" = "s3" ]; then
-    aws s3 cp postgresql-*.tar s3://$BUCKET_NAME/
-else 
-    echo "remote backup not specified"    
+  aws s3 cp "/tmp/$POSTGRESQL_BACKUP_DIR/postgresql-$DATE.tar" \
+    "s3://$BUCKET_NAME/"
+else
+  echo "Remote backup not specified."
 fi
 
 # Delete the temp directory
-rm -rf /tmp/"$POSTGRESQL_BACKUP_DIR"/
+rm -rf "/tmp/$POSTGRESQL_BACKUP_DIR"
 
-# Cleanup the old backup
-find $VOLUME_DIR -mindepth 1 -mtime +$BACKUP_TIME -delete
+# Clean up old backups
+find "$VOLUME_DIR" -type f -name "postgresql-*.tar" -mtime +$BACKUP_TIME -delete
