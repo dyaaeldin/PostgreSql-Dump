bc297ce7357bc4e3069192bc6d642f9cc3416184
Dyaa Ahmed <79382618+dyaaeldin@users.noreply.github.com>
2023-03-21 07:38:56 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env sh
 
-# Configure daily cronjob
-echo "$MONGODB_CRON_PATTERN /usr/local/bin/postgresql.sh" >> /etc/crontabs/root 
+# Add cron job to run the PostgreSQL backup script
+echo "$POSTGRESQL_CRON_PATTERN /usr/local/bin/postgresql.sh" >> /etc/crontabs/root 
+
+# Ensure the backup script is executable
 chmod a+x /usr/local/bin/postgresql.sh
 
-# Start the crond service
-crond -f 
+# Start the crond service and run it in the foreground
+crond -f
